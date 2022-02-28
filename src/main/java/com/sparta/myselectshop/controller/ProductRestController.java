5e118ba579bb1dc28c0d4d224cc2fbc5bd91a266
yanJuicy <hanjub3@gmail.com>
2022-02-28 17:29:29 +0900

    update가 안되는 오류 해결

--- src/main/java/com/sparta/myselectshop/controller/ProductRestController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductRestController.java
@@ -1,11 +1,15 @@
 package com.sparta.myselectshop.controller;
 
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.repository.ProductRepository;
+import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +20,7 @@ import java.util.List;
 public class ProductRestController {
 
     private final ProductRepository productRepository;
+    private final ProductService productService;
 
     // 전체 상품 조회
     @GetMapping("/api/products")
@@ -29,6 +34,12 @@ public class ProductRestController {
         Product product = new Product(requestDto);
         productRepository.save(product);
         return product;
+    }
+
+    // 설정 가격 변경
+    @PutMapping("/api/products/{id}")
+    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
+        return productService.update(id, requestDto);
     }
 
 }
